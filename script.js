ee7e7fb6e701276bbb75f47cd41f6459809ace8e
tahis9 <147927771+tahis9@users.noreply.github.com>
2025-01-27 18:52:03 +1100

    Create base function for game and gave up on pseudocode

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function getComputerChoice() {
   // Return string
   return choice;
 }
+// console.log(getComputerChoice())
 
 function getHumanChoice() {
   // Create variable to store user choice
@@ -19,6 +20,68 @@ function getHumanChoice() {
   return choice;
 }
 
+// Create variable to track Player's score
+let humanScore = 0;
 
-console.log(getComputerChoice())
+// Create var to track Computer's score
+let computerScore = 0;
+
+function calcResult(humanChoice,computerChoice) {
+  let result = "";
+  if (humanChoice == "rock") {
+    if (computerChoice == "paper") {
+      result = "lose"
+    } else if (computerChoice == "scissors") {
+      result = "win"
+    } 
+  }
+  if (humanChoice == "paper") {
+    if (computerChoice == "scissors") {
+      result = "lose"
+    } else if (computerChoice == "rock") {
+      result = "win"
+    }
+  } 
+  if (humanChoice == "scissors") {
+    if (computerChoice == "rock") {
+      result = "lose"
+    } else if (computerChoice == "paper") {
+      result = "win"
+    }
+  } 
+  if (result == "") {
+    result = "drew" }
+  return result;
+}
+
+function calcResultChoices(a,b) {
+  if (a === b) {
+    return (a + " draws with itself");
+  } else {
+    let result = calcResult(a,b)
+    if (result == "win") {
+      return (a + " beats " + b);
+    } else {
+      return (b + " beats " + a);
+    }
+    
+  }
+  
+}
+
+
+function playRound() {
+  let humanChoice = getHumanChoice().toLowerCase();
+  let computerChoice = getComputerChoice().toLowerCase(); 
+  result = calcResult(humanChoice,computerChoice);
+  console.log("You " + result + "! " + calcResultChoices(humanChoice,computerChoice));
+  if (result === "win") {
+    humanScore += 1;
+  }
+  if (result === "lose") {
+    computerScore += 1;
+  }
+}
+
+
 
