82b8b19d059e06510dab73932967331ae87c0ef1
tahis9 <147927771+tahis9@users.noreply.github.com>
2025-01-27 17:30:25 +1100

    Create functions for choices

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Hello World!");
+
+function getComputerChoice() {
+  // Create Random Number
+  let randDec = Math.random();
+  // Convert Random number to integer of 1, 2 or 3
+  let randInt = Math.ceil(randDec*3);
+  // Convert Int to Rock, Paper or Scissors
+  let choice = (randInt === 1) ? "Rock" :
+    (randInt === 2) ? "Paper" : "Scissors";
+  // Return string
+  return choice;
+}
+
+function getHumanChoice() {
+  // Create variable to store user choice
+  let choice = "";
+  // Ask user for their choice
+  choice = window.prompt("Do you choose Rock, Paper or Scissors?");
+  return choice;
+}
+
+
+console.log(getComputerChoice())
+
